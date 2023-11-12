710611401b2b88a1cae4e521f7dcb0396d03bd5b
ultrasonicytb <ultrasonic2408@gmail.com>
2023-11-12 23:11:31 +0100

    Update Point.cpp

--- Prosit-1/Prosit-1/Point.cpp
+++ Prosit-1/Prosit-1/Point.cpp
@@ -12,10 +12,16 @@ double Point::distance(const Point& a){
 
 bool Point::entre(const Point& a, const Point& b){
 	// y = mx + p
-	// Recuperation du coef directeur
+
+	// Si la droite est verticale
 	if (b.getX() - a.getX() == 0) {
-		return 0;
+		// Si le x est le meme et que y est compris entre le min et max y
+		return (x == a.getX() &&
+			((y >= a.getY() && y <= b.getY()) || (y >= b.getY() && y <= a.getY()))
+		);
 	}
+
+	// Recuperation du coef directeur
 	double m = (b.getY() - a.getY()) / (b.getX() - a.getX());
 
 	// Recuperation de l'ordonnée a l'origine
